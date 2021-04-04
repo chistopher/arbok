42d91cf5f126f2071469ba9af9f7ac8a3bdb9b18
Otto Kissig <1622854+EightSQ@users.noreply.github.com>
2021-04-04 21:43:53 +0200

    Start migration of Smaller Into Larger Variant
    Co-authored-by: Maximilian Böther <maxi+git@boether.de>

---
+++ source/apps/fastestspeedrun.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+#include <arbok/tarjan.h>
+
+using namespace std;
+
+// This application should solve the following codeforces task
+// https://codeforces.com/gym/102483/problem/F
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n;
+  cin >> n;
+  Tarjan<int, int> tarjan(n + 1);
+  // G.resize(n + 1);
+  for (int level = 1; level <= n; level++) {
+    int shortcut_item, time;
+    cin >> shortcut_item >> time;
+    tarjan.create_edge(shortcut_item, level, time);
+    for (int item = 0; item <= n; item++) {
+      cin >> time;
+      if (item != shortcut_item) {
+        tarjan.create_edge(item, level, time);
+      }
+    }
+  }
+
+  tarjan.run(0);
+  cout << tarjan.weight() << endl;
+
+  return 0;
+}
--- source/arbok/include/arbok/tarjan.h
+++ source/arbok/include/arbok/tarjan.h
@@ -1,6 +1,41 @@
-
+#pragma once
+#include <cstdint>
 #include <vector>
 
-void tarjan_treap();
-void tarjan_set();
-void tarjan_matrix();
+namespace arbok {
+
+template <class VertexIdentifier = uint32_t, class Weight = int32_t>
+class Tarjan {
+ public:
+  Tarjan(VertexIdentifier) = delete;
+  virtual ~Tarjan(){};  // virtual destructors need implementations
+  virtual void create_edge(VertexIdentifier, VertexIdentifier, Weight) = 0;
+  void run() final;
+  Weight weight() const;
+
+ protected:
+  VertexIndentifier _num_vertices;
+};
+
+template <class VertexIdentifier = uint32_t, class Weight = int32_t>
+class SetTarjan : public Tarjan {
+ public:
+  SetTarjan() = default;
+  SetTarjan(VertexIdentifier);
+  void create_edge(VertexIdentifier, VertexIdentifier, Weight) override;
+
+ private:
+  vector<vector<edge>> _graph;
+}
+
+template <class VertexIdentifier = uint32_t, class Weight = int32_t>
+class MatrixTarjan : public Tarjan {
+ public:
+  MatrixTarjan() = default;
+  MatrixTarjan(VertexIdentifier);
+  void create_edge(VertexIdentifier, VertexIdentifier, Weight) override;
+}
+
+// TODO: TreapTarjan
+
+}  // end namespace arbok
