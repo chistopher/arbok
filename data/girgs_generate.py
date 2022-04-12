42a9c71819ba1a41946e00c565d7d02b09d18783
Christopher Weyand <christopher.weyand@hpi.de>
2022-04-12 11:26:31 +0200

    girgs

--- data/girgs_generate.py
+++ data/girgs_generate.py
@@ -2,14 +2,25 @@
 
 import os
 import subprocess
+import random
 
+random.seed(1337)
 girg_cli = "../../girgs/build/gengirg"
 os.makedirs("girgs", exist_ok=True)
 logfile = open('results/girgs_log', 'w')
 
-n = 10**5
-for deg in range(50,1001,50):
-    print(deg)
-    clicall = [girg_cli, '-n', str(n), '-deg', str(deg), '-edge', '1', '-file', f'girgs/girg{deg:05d}']
-    subprocess.run(clicall, stdout=logfile)
+n = 10**4
+for deg in range(100,2001,100):
+    for i in range(10):
+        print(deg, i)
+        clicall = [girg_cli, 
+            '-n', str(n), 
+            '-deg', str(deg), 
+            '-edge', '1', 
+            '-file', f'girgs/girg{deg:04d}_{i}',
+            '-pseed', str(random.randint(0,10**6)), 
+            '-aseed', str(random.randint(0,10**6)), 
+            '-sseed', str(random.randint(0,10**6)), 
+            ]
+        subprocess.run(clicall, stdout=logfile)
 
